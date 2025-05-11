8eebc4c87dcd77977887468d9f737012889a47f6
Haroldo Ramirez da Nobrega <haroldoramirez@gmail.com>
2025-05-11 01:15:53 -0300

    iniciado regra javascript

--- src/main/resources/static/js/register/create.js
+++ src/main/resources/static/js/register/create.js
@@ -1,1 +1,33 @@
-console.log("create.js")
+window.onload = function() {
+
+    console.log("create.js");
+
+    const abrirModalConfirmacao = document.getElementById("registrar");
+    const cancelar = document.getElementById("cancelar");
+
+    let modalConfirmacao = new bootstrap.Modal(document.getElementById("confirmarRegistro"));
+
+    abrirModalConfirmacao.addEventListener('click', function () {
+
+        let formularioRegistro = document.getElementById("formularioRegistro");
+
+        let name = formularioRegistro.name.value;
+        let kills = formularioRegistro.kills.value;
+
+        document.getElementById("paragrafoName").innerHTML = name;
+        document.getElementById("paragrafoKills").innerHTML = kills;
+
+        modalConfirmacao.show();
+
+    });
+
+
+    function fecharModal() {
+        modalConfirmacao.hide();
+    }
+
+    cancelar.addEventListener('click', function () {
+        window.location.href = "/";
+    });
+
+}
